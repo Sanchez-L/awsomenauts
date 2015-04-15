757e068cf648c25d201b6bf87a5b1b42f03c988e
leonardo sanchez <leonardodicaprip831@yahoo.ocm>
2015-04-15 09:21:30 -0700

    4/15/15

--- js/resources.js
+++ js/resources.js
@@ -11,6 +11,7 @@ game.resources = [
         {name: "tower", type:"image", src: "data/img/tower_round.svg.png"},
         {name: "creep1", type:"image", src: "data/img/brainmonster.png"},
         {name: "title-screen", type:"image", src: "data/img/title.png"},
+        {name: "image2", type:"image", src: "data/img/image2.png"},
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
